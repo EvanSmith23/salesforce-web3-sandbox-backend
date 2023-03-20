ac8a66d760b642d0ee97ee2ab4b64adbc174951e
Evan Smith <smith.e@salesforce.com>
2023-03-20 15:36:30 -0500

    error fixing changes

--- database/knexfile.js
+++ database/knexfile.js
@@ -25,4 +25,16 @@ module.exports = {
     },
     debug: true,
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+    debug: true,
+  },
 };
